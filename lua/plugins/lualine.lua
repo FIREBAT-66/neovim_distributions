6888864de4c7110088ac63533609f157af96e6d6
ghostnation66 <66.ghostnation@gmail.com>
2025-08-24 11:14:47 -0600

    |.git| = 1648

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,100 @@
+return {
+    'nvim-lualine/lualine.nvim',
+    config = function()
+      local mode = {
+        'mode',
+        fmt = function(str)
+          return ' ' .. str
+          -- return ' ' .. str:sub(1, 1) -- displays only the first character of the mode
+        end,
+      }
+
+      local filename = {
+        'filename',
+        file_status = true, -- displays file status (readonly status, modified status)
+        path = 0, -- 0 = just filename, 1 = relative path, 2 = absolute path
+      }
+
+      local hide_in_width = function()
+        return vim.fn.winwidth(0) > 100
+      end
+
+      local diagnostics = {
+        'diagnostics',
+        sources = { 'nvim_diagnostic' },
+        sections = { 'error', 'warn' },
+        symbols = { error = ' ', warn = ' ', info = ' ', hint = ' ' },
+        colored = false, -- Disable colored output
+        update_in_insert = false,
+        always_visible = false,
+        cond = hide_in_width,
+      }
+
+      local diff = {
+        'diff',
+        colored = false,
+        symbols = { added = ' ', modified = ' ', removed = ' ' }, -- changes diff symbols
+        cond = hide_in_width,
+      }
+      local my_custom_theme = {
+        normal = {
+          a = { fg = '#2c2c2c', bg = '#ff6000', gui = 'bold' },
+          b = { fg = '#ff6000', bg = '#2c2c2c' , blend = 60 },
+          c = { fg = '#2b2b2b', bg = '#80401a' },
+        },
+        insert = {
+          a = { fg = '#2c2c2c', bg = '#ff6000', gui = 'bold' },
+          b = { fg = '#ff6000', bg = '#2c2c2c' , blend = 60 },
+          c = { fg = '#2b2b2b', bg = '#80401a' },
+        },
+        visual = {
+          a = { fg = '#2c2c2c', bg = '#ff6000', gui = 'bold' },
+          b = { fg = '#ff6000', bg = '#2c2c2c' , blend = 60 },
+          c = { fg = '#2b2b2b', bg = '#80401a' },
+        },
+        replace = {
+          a = { fg = '#2c2c2c', bg = '#ff6000', gui = 'bold' },
+          b = { fg = '#ff6000', bg = '#2c2c2c' , blend = 60 },
+          c = { fg = '#2b2b2b', bg = '#80401a' },
+        },
+        inactive = {
+          a = { fg = '#ff6000', bg = '#80401a', gui = 'bold' },
+          b = { fg = '#ff6000', bg = '#2b2b2b' },
+          c = { fg = '#ff6000', bg = '#80401a' },
+        },
+      }
+
+      require('lualine').setup {
+        options = {
+          icons_enabled = true,
+          theme = my_custom_theme, -- Set theme based on environment variable
+          -- Some useful glyphs:
+          -- https://www.nerdfonts.com/cheat-sheet
+          --        
+          section_separators = { left = '', right = '' },
+          component_separators = { left = '', right = '' },
+          disabled_filetypes = { 'alpha', 'neo-tree'},
+          always_divide_middle = true,
+        },
+        sections = {
+          lualine_a = { mode },
+          lualine_b = { 'branch' },
+          lualine_c = { filename },
+          lualine_x = { diagnostics, diff, { 'encoding', cond = hide_in_width }, { 'filetype', cond = hide_in_width } },
+          lualine_y = { 'location' },
+          lualine_z = { 'progress' },
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = { { 'filename', path = 1 } },
+          lualine_x = { { 'location', padding = 0 } },
+          lualine_y = {},
+          lualine_z = {},
+        },
+        tabline = {},
+        extensions = { 'fugitive' },
+      }
+    end,
+  }
+
